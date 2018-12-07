c585ee83e3133c25629356664b859c6889969cf8
Nicolas Poncet <nicolas.poncet1@gmail.com>
2018-12-07 18:08:52 +0000

    Initial commit

---
+++ app/src/main/java/com/example/cpu150/randomusers/activities/HomePageActivity.kt
@@ -0,0 +1,13 @@
+package com.example.cpu150.randomusers.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cpu150.randomusers.R
+
+class HomePageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_page)
+    }
+}
