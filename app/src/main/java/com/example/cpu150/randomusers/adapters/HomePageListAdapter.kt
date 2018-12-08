e8057ef0b1d59d58f262641fed601990c38d16ee
Nicolas Poncet <nicolas.poncet1@gmail.com>
2018-12-08 21:00:47 +0000

    Added Picasso + implement the profile picture download

--- app/src/main/java/com/example/cpu150/randomusers/adapters/HomePageListAdapter.kt
+++ app/src/main/java/com/example/cpu150/randomusers/adapters/HomePageListAdapter.kt
@@ -5,9 +5,11 @@ import android.support.constraint.ConstraintLayout
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.ImageView
 import com.example.cpu150.randomusers.R
 import com.example.cpu150.randomusers.models.RandomUserModel
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.card_view_home_page.view.*
 
 class HomePageListAdapter (private val dataList: List <RandomUserModel?>?, private val context: Context) : RecyclerView.Adapter<HomePageListAdapter.HomePageListViewHolder>() {
 
@@ -15,8 +17,11 @@ class HomePageListAdapter (private val dataList: List <RandomUserModel?>?, priva
         // Create new card view
         val cardView = LayoutInflater.from(p0.context).inflate(R.layout.card_view_home_page, p0, false) as ConstraintLayout
 
-        // Set view
-        cardView.findViewById<TextView?>(R.id.nameTextView)?.text = getName (dataList?.get(p1))
+        // Setup card
+        dataList?.get(p1)?.let {
+            cardView.nameTextView.text = getName (it)
+            updateAvatar(it, cardView.avatarImageView)
+        }
 
         return HomePageListViewHolder(cardView)
     }
@@ -27,7 +32,8 @@ class HomePageListAdapter (private val dataList: List <RandomUserModel?>?, priva
 
     override fun onBindViewHolder(p0: HomePageListViewHolder, p1: Int) {
         // Update card
-        p0.itemView.findViewById<TextView?>(R.id.nameTextView)?.text = getName (dataList?.get(p1))
+        p0.itemView.nameTextView.text = getName (dataList?.get(p1))
+        updateAvatar(dataList?.get(p1), p0.itemView.avatarImageView)
     }
 
     class HomePageListViewHolder(cardView: ConstraintLayout) : RecyclerView.ViewHolder(cardView)
@@ -37,5 +43,13 @@ class HomePageListAdapter (private val dataList: List <RandomUserModel?>?, priva
             context.getString(R.string.home_page_card_name),
             userModel?.name?.first ?: "",
             userModel?.name?.last ?: "")
+    }
+
+    private fun updateAvatar (userModel: RandomUserModel?, avatarImageView: ImageView) {
+        Picasso
+            .get()
+            .load(userModel?.picture?.large?.toString())
+            .placeholder(R.drawable.tux)
+            .into(avatarImageView)
     }
 }
