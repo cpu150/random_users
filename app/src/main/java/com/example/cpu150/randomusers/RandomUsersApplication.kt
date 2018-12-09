bb1d4daf9116d2bbb6558cd70afae28e1608a4bd
Nicolas Poncet <nicolas.poncet1@gmail.com>
2018-12-09 16:51:21 +0000

    Added OkHttp examples

--- app/src/main/java/com/example/cpu150/randomusers/RandomUsersApplication.kt
+++ app/src/main/java/com/example/cpu150/randomusers/RandomUsersApplication.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import com.example.cpu150.randomusers.network.RandomUserApi
 import com.squareup.picasso.OkHttp3Downloader
 import okhttp3.*
+import java.util.concurrent.TimeUnit
 
 class RandomUsersApplication : Application() {
     override fun onCreate() {
@@ -20,14 +21,40 @@ class RandomUsersApplication : Application() {
         val httpLoggingInterceptor = HttpLoggingInterceptor(HttpLoggingInterceptor.Logger { message -> Log.d("HttpLog", message) })
         httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
 
-        val okHttpClient = OkHttpClient()
+        val okHttpBuilder = OkHttpClient()
             .newBuilder()
             .cache(cache)
             .addInterceptor(httpLoggingInterceptor)
-            .build()
 
+        addHeadersExample(okHttpBuilder)
+
+        setupTimeoutExample(okHttpBuilder)
+
+        val okHttpClient = okHttpBuilder.build()
         val okHttpDownloader = OkHttp3Downloader(okHttpClient)
 
         RandomUserApi.init(okHttpClient, okHttpDownloader)
+    }
+
+    private fun addHeadersExample (okHttpBuilder: OkHttpClient.Builder)
+    {
+        okHttpBuilder.addInterceptor { chain ->
+            val original = chain.request()
+            val requestBuilder = original.newBuilder()
+                .addHeader("Accept", "application/json")
+                .addHeader("Content-Type", "application/json")
+
+            chain.proceed(requestBuilder.build())
+        }
+    }
+
+    private fun setupTimeoutExample (okHttpBuilder: OkHttpClient.Builder)
+    {
+        val requestTimeout = 60L
+
+        okHttpBuilder
+            .connectTimeout(requestTimeout, TimeUnit.SECONDS)
+            .readTimeout(requestTimeout, TimeUnit.SECONDS)
+            .writeTimeout(requestTimeout, TimeUnit.SECONDS)
     }
 }
