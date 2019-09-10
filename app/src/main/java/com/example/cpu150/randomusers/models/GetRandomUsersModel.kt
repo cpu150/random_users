7395287cb25b208f53d02a372b8eb7ab6bfaeddd
Nicolas Poncet <nicolas.poncet1@gmail.com>
2019-09-10 16:49:16 +0100

    Added annotaions (as an example): - Retrofit @Header annotation - Gson @Expose annotation

--- app/src/main/java/com/example/cpu150/randomusers/models/GetRandomUsersModel.kt
+++ app/src/main/java/com/example/cpu150/randomusers/models/GetRandomUsersModel.kt
@@ -1,5 +1,9 @@
 package com.example.cpu150.randomusers.models
 
+import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
-data class GetRandomUsersModel (@SerializedName("results") val results: List<RandomUserModel>?)
+data class GetRandomUsersModel (
+    @Expose(serialize = true, deserialize = true)
+    @SerializedName("results")
+    val results: List<RandomUserModel>?)
--- app/src/main/java/com/example/cpu150/randomusers/network/RandomUserEndpoints.kt
+++ app/src/main/java/com/example/cpu150/randomusers/network/RandomUserEndpoints.kt
@@ -3,9 +3,11 @@ package com.example.cpu150.randomusers.network
 import com.example.cpu150.randomusers.models.GetRandomUsersModel
 import io.reactivex.Single
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.Query
 
 interface RandomUserEndpoints {
+    @Headers("Content-Type:application/json; Accept:application/json")
     @GET("?format=json")
     fun getRandomUsers (@Query("results") numberOfUser: Int): Single<GetRandomUsersModel>
 }
